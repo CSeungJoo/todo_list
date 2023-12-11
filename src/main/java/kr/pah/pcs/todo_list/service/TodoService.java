b186a6d3a63c77f3abdff851b6908c52c83ede01
CSeungJoo <seungjoo13579@gmail.com>
2023-12-11 22:12:35 +0900

    todo 전체 조회 구현

--- src/main/java/kr/pah/pcs/todo_list/service/TodoService.java
+++ src/main/java/kr/pah/pcs/todo_list/service/TodoService.java
@@ -1,11 +1,18 @@
 package kr.pah.pcs.todo_list.service;
 
+import kr.pah.pcs.todo_list.domain.Todo;
 import kr.pah.pcs.todo_list.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TodoService {
     private final TodoRepository todoRepository;
+
+    public List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
 }
